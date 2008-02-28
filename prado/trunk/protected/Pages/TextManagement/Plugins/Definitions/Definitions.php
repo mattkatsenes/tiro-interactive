df89a77f38ea89fdd951d1a2c121d78280908a6d
psalakanthos <psalakanthos@ea5101dd-cf45-0410-a327-c316ea116008>
2008-02-28 11:57:50 +0000

    A helping hand for DB.  Definitions page now loads up the XML file it is called to mess about with.  You might want to copy ExampleXML.xml into that file.

--- prado/trunk/protected/Pages/TextManagement/Plugins/Definitions/Definitions.php
+++ prado/trunk/protected/Pages/TextManagement/Plugins/Definitions/Definitions.php
@@ -1,11 +1,27 @@
 <?php
+/**
+ * Get the TiroText class (and the rest of that directory) involved.
+ */
+prado::Using('Application.Engine.*');
 
 class Definitions extends TPage
 {
 
-	
+	public $tiroText;
 
-
+	/**
+	 * This runs as soon as the page loads.
+	 * 
+	 * The class constructor is gobbled up by the "TPage" class, but it
+	 * calls this baby, so fo our purposes, this is equivalent.
+	 */
+	function onLoad()
+	{
+		global $ABS_PATH,$USERS_PREFIX;
+		$dbRecord = TextRecord::finder()->findByPk($_GET['id']);
+		$path = $ABS_PATH.'/'.$USERS_PREFIX.'/'.$this->User->Name.'/'.$dbRecord->dir_name;
+		$this->tiroText = new TiroText($ABS_PATH.'/'.$USERS_PREFIX.'/'.$this->User->Name.'/'.$dbRecord->dir_name);
+	}
 
 	function Load()
 	{
