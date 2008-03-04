a4267204b07d7513402209c07329ed09bf1f1fc9
Drew <drewbuschhorn@gmail.com>
2008-03-04 06:20:32 +0000

    Modified the defJSONHandler to create <entry> xml elements for the glossary xml.

--- prado/trunk/protected/Pages/TextManagement/Plugins/Definitions/Definitions.php
+++ prado/trunk/protected/Pages/TextManagement/Plugins/Definitions/Definitions.php
@@ -8,6 +8,7 @@ class Definitions extends TPage
 {
 
 	public $tiroText;
+	private $localDictionary = new DOMDocument();
 
 	/**
 	 * This runs as soon as the page loads.
@@ -39,7 +40,7 @@ class Definitions extends TPage
 	
 	$textToHtmlSheet = new DOMDocument();
 		$textToHtmlSheet->load('protected/Pages/TextManagement/Plugins/Definitions/tirotext_label.xsl');
-	$proc = new XSLTProcessor;
+	$proc = new XSLTProcessor();
 		$proc->importStyleSheet($textToHtmlSheet); // attach the xsl rules
 	$textXML->loadXML($proc->transformToXML($textXML));	
 	
@@ -67,9 +68,52 @@ class Definitions extends TPage
         $xml = base64_decode($json['xml']);
         $xml = gzuncompress($xml);
         
-	   //Test of xml transfer, to be removed.
-        $this->DefinitionAnchor->Controls[]="<![CDATA[". $xml ."]]>";
-	}
+	//	Test of xml transfer, to be removed.
+	//	$this->DefinitionAnchor->Controls[]="<![CDATA[". $xml ."]]>";
 	
+	$localDictionary->loadXML('<div n="glossary">');
+	
+	$sourceXML 		= new DOMDocument();
+	$sourceXML->loadXML($xml);
+		$xpath = new DOMXPath($sourceXML);
+	
+		$entry = $localDictionary->createElement('entry');
+			//
+			$lemma = $xpath->query('//Definition/@lemma')->item(0);
+			$entry->setAttribute('xml:id','g.' . $lemma->value);
+			//
+			//
+			$key = $xpath->query('//Definition/entry/@key')->item(0);
+			$entry->setAttribute('els_key', $key->value);
+			//
+			//
+			$form = $xpath->query('//Definition/entry/form')->item(0);
+			$orth = $form->getElementsByTagName('orth')->item(0);
+				$itype = $xpath->query('//Definition/entry/gramGrp/itype')->item(0);
+			$orth->nodeValue = trim($orth->nodeValue) . ", " . trim($itype->nodeValue);
+			$entry->appendChild($localDictionary->importNode($form,true));
+			//
+			//
+			$gramGrp = $localDictionary->createElement('gramGrp');
+			$gramSubs = $xpath->query('//Definition/entry/gramGrp/gen');	
+				for($i=0; $i < $gramSubs->length; $i++)
+					$gramGrp->appendChild($localDictionary->importNode($gramSubs->item($i),true));
+			$gramPos = $xpath->query('//Definition/morphAnalysis')->item(0);
+			$gramPos = explode(' ',$gramPos->nodeValue);
+			$gramPos = $gramPos[0];
+				$gramPosNode = $localDictionary->createElement('pos');
+				$gramPosNode->nodeValue = trim($gramPos);
+			$gramGrp->appendChild($gramPosNode);
+			$entry->appendChild($gramGrp);			
+			//
+	
+	$definitionNode = $localDictionary->createElement('def');
+	$definitionNode->nodeValue = trim($json['userdefinition']);
+	$entry->appendChild($defintionNode);
+	$localDictionary->appendChild($entry);
+	
+	$this->DefinitionAnchor->Controls[]="<![CDATA[". $localDictionary->saveXML() ."]]>";
+	}
 }
+
 ?>
