8f714b06a1277932af6526caedd4a8f1ac860122
psalakanthos <psalakanthos@ea5101dd-cf45-0410-a327-c316ea116008>
2008-03-19 14:30:19 +0000

    Images implementation begun.  Cannot yet upload images.

---
+++ prado/trunk/protected/Pages/TextManagement/Plugins/Images/js/expander.js
@@ -0,0 +1,31 @@
+Event.observe(window, 'load', init, false);
+
+function init()
+{
+	branches = $('tiro_').select('div');
+	
+	branches.invoke('hide');
+}
+
+function toggleExpand(branch_id)
+{
+	children = $(branch_id).childElements();
+
+	children.each(function(child){
+		if(child.nodeName == 'A')
+			child.descendants().each(function(kiddie){
+				if(kiddie.src.search(/east/) > -1)
+					kiddie.src = 'http://localhost/frontend/images/south.gif';
+				else
+					kiddie.src = 'http://localhost/frontend/images/east.gif';
+			});
+		else
+			child.toggle();
+	});
+	
+}
+
+function attach(tiro_id)
+{
+	
+}
