22f51605002c03248eb57dd41d69a0c4133e6e52
psalakanthos <psalakanthos@ea5101dd-cf45-0410-a327-c316ea116008>
2008-04-04 12:50:13 +0000

    A bit of snazzy JS.

--- prado/trunk/protected/Data/js/annotator.js
+++ prado/trunk/protected/Data/js/annotator.js
@@ -5,14 +5,14 @@ function attachAnnotation(tiro_id)
 {
 	if($(tiro_id).hasClassName('leaf'))
 	{
-		$(tiro_id).setStyle({fontWeight: 'bold'});
-		new Effect.Shake(tiro_id);
+		$(tiro_id).toggleClassName('annotated');
+//		new Effect.Shake(tiro_id);
 		annotateLeaf(tiro_id);
 	}
 	else
 	{
-		$(tiro_id).setStyle({fontStyle: 'italics'});
-		new Effect.Shake(tiro_id);
+		$(tiro_id).toggleClassName('annotated_line');
+//		new Effect.Shake(tiro_id);
 		annotateBranch(tiro_id);
 	}
 };
@@ -20,6 +20,9 @@ function attachAnnotation(tiro_id)
 annotateLeaf = function(tiro_id){
 	var content = "<h2>Annotation type:</h2><p>"+$(tiro_id).innerHTML+"<ul><li>Type 1</li><li>Type 2</li></ul></p>";
 	$('annotationBox').update(content);
+	$('annotationBox').clonePosition($(tiro_id),{setWidth:false, setHeight:false,offsetLeft:100,offsetTop:-80});
+	$('annotationBox').show();
+	new Effect.BlindDown('annotationBox');
 };
 
 annotateBranch = function(tiro_id){
