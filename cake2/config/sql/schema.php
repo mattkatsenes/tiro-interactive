db38c2edb860200d0f266b5a1340bfb01f1eb895
psalakanthos <psalakanthos@ea5101dd-cf45-0410-a327-c316ea116008>
2008-06-05 15:13:42 +0000

    Setup Complete. To set up the database on another computer, run cake schema run create <db_name>.
    The cake files are set to be in /etc/cake2/cake

--- cake2/config/sql/schema.php
+++ cake2/config/sql/schema.php
@@ -1,11 +1,11 @@
 <?php 
 /* SVN FILE: $Id: schema.php 6311 2008-01-02 06:33:52Z phpnut $ */
-/*App schema generated on: 2008-05-31 17:05:58 : 1212270238*/
+/*TiroCake schema generated on: 2008-06-05 11:06:08 : 1212678308*/
 
 
-class AppSchema extends CakeSchema {
+class TiroCakeSchema extends CakeSchema {
 
-	var $name = 'App';
+	var $name = 'TiroCake';
 
 	function before($event = array()) {
 		return true;
@@ -14,62 +14,26 @@ class AppSchema extends CakeSchema {
 	function after($event = array()) {
 	}
 
-	var $acos = array(
-			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-			'parent_id' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'model' => array('type'=>'string', 'null' => true),
-			'foreign_key' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'alias' => array('type'=>'string', 'null' => true),
-			'lft' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'rght' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+	var $groups = array(
+			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+			'created' => array('type'=>'datetime', 'null' => true, 'default' => NULL),
+			'modified' => array('type'=>'datetime', 'null' => true, 'default' => NULL),
+			'name' => array('type'=>'string', 'null' => false, 'length' => 32),
 			'indexes' => array()
 		);
 
-	var $aros = array(
-			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-			'parent_id' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'model' => array('type'=>'string', 'null' => true),
-			'foreign_key' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'alias' => array('type'=>'string', 'null' => true),
-			'lft' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'rght' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'indexes' => array()
-		);
-
-	var $aros_acos = array(
-			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-			'aro_id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-			'aco_id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-			'_create' => array('type'=>'string', 'null' => false, 'default' => '0', 'length' => 2),
-			'_read' => array('type'=>'string', 'null' => false, 'default' => '0', 'length' => 2),
-			'_update' => array('type'=>'string', 'null' => false, 'default' => '0', 'length' => 2),
-			'_delete' => array('type'=>'string', 'null' => false, 'default' => '0', 'length' => 2),
-			'indexes' => array()
-		);
-
-	var $chunks = array(
-			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-			'project_id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-			'text' => array('type'=>'text', 'null' => true, 'default' => NULL),
-			'type' => array('type'=>'string', 'null' => false, 'default' => NULL, 'length' => 30),
-			'index' => array('type'=>'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-			'indexes' => array()
-		);
-
-	var $projects = array(
-			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-			'name' => array('type'=>'string', 'null' => false, 'default' => NULL, 'length' => 100),
-			'user_id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-			'dir_name' => array('type'=>'string', 'null' => false, 'default' => NULL, 'length' => 50),
-			'author' => array('type'=>'string', 'null' => true, 'default' => NULL, 'length' => 100),
+	var $groups_users = array(
+			'group_id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+			'user_id' => array('type'=>'integer', 'null' => false, 'default' => NULL),
 			'indexes' => array()
 		);
 
 	var $users = array(
-			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-			'login' => array('type'=>'string', 'null' => false, 'default' => NULL, 'length' => 50),
-			'password' => array('type'=>'string', 'null' => false, 'default' => NULL, 'length' => 100),
-			'role' => array('type'=>'integer', 'null' => false, 'default' => '1', 'length' => 3),
+			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+			'created' => array('type'=>'datetime', 'null' => true, 'default' => NULL),
+			'modified' => array('type'=>'datetime', 'null' => true, 'default' => NULL),
+			'username' => array('type'=>'string', 'null' => false, 'length' => 16),
+			'password' => array('type'=>'string', 'null' => false, 'length' => 32),
 			'indexes' => array()
 		);
 
